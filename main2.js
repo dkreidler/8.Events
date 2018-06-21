599fb0cb7809b976ecbb420eeb17380eb71a59ab
dkreidler <dkreidler@gmail.com>
2018-06-20 22:06:03 -0400

    Addition of event object (e)

--- main2.js
+++ main2.js
@@ -1,11 +1,17 @@
-    var btn = document.querySelector('button');
+var btn = document.querySelector('button');
 
-    function random(number) {
-        return Math.floor(Math.random() * number);
-    }
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
 
-    function bgChange() {
-        var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-        document.body.style.backgroundColor = rndCol;
-    }
-    btn.onclick = bgChange;
+/*function bgChange() {
+    var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    document.body.style.backgroundColor = rndCol;
+}*/
+function bgChange(e) {
+    var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    e.target.style.backgroundColor = rndCol;
+    console.log(e);
+}
+btn.addEventListener('click', bgChange);
+// btn.onmouseout = bgChange;
