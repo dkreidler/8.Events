773f16b5d3e62568aa52d0fca5486dd52a6624b6
dkreidler <dkreidler@gmail.com>
2018-06-20 21:24:43 -0400

    other event handler methods

---
+++ main2.js
@@ -0,0 +1,11 @@
+    var btn = document.querySelector('button');
+
+    function random(number) {
+        return Math.floor(Math.random() * number);
+    }
+
+    function bgChange() {
+        var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+        document.body.style.backgroundColor = rndCol;
+    }
+    btn.onclick = bgChange;
