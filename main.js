d177c7f18ad7dd41b59c1d644f415ff4d453abda
dkreidler <dkreidler@gmail.com>
2018-06-20 21:08:24 -0400

    Created an eyesore...

--- main.js
+++ main.js
@@ -5,5 +5,12 @@ function random(number) {
 }
 btn.onclick = function() {
     var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    var rndCol2 = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    var rndCol3 = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    var rndSize = random(100) + 'px';
     document.body.style.backgroundColor = rndCol;
+    document.getElementById('button').style.background = rndCol2;
+    document.getElementById('button').style.color = rndCol3;
+    document.getElementById('button').style.fontSize = rndSize;
+
 }
