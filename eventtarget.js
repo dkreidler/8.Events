5dfc8b6cafb337a4c018e8d9f62a3b1f0790f192
dkreidler <dkreidler@gmail.com>
2018-06-20 22:48:59 -0400

    Refactored into separate files.
    html, js & css

---
+++ eventtarget.js
@@ -0,0 +1,20 @@
+for (var i = 1; i <= 5000; i++) {
+    var myDiv = document.createElement('div');
+    document.body.appendChild(myDiv);
+}
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function bgChange() {
+    var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    return rndCol;
+}
+var divs = document.querySelectorAll('div');
+
+for (var i = 0; i < divs.length; i++) {
+    divs[i].onmouseenter = function(e) {
+        e.target.style.backgroundColor = bgChange();
+    };
+}
